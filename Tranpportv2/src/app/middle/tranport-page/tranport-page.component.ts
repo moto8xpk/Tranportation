b3613c1be9c5ad24ac54be9ec5d4c606a4276ac6
moto8xpk <35144476+moto8xpk@users.noreply.github.com>
2019-01-29 14:37:08 +0700

    commit
    commit import form vs sub menu button
    activities, data management

--- Tranpportv2/src/app/middle/tranport-page/tranport-page.component.ts
+++ Tranpportv2/src/app/middle/tranport-page/tranport-page.component.ts
@@ -14,16 +14,16 @@ export class TranportPageComponent implements OnInit {
 
   ngOnInit() {
     this.tabList=[
-      { id:0,link:'/tranport/buses-management',title:"Buses Management",disable:true},
-      {id:1,link:'/tranport/auto-setup',title:"Auto Setup",disable:false},
-      {id:2,link:'/tranport/detail-list',title:"Detail List",disable:false},
-      {id:3,link:'/tranport/notice-change',title:"Notice Change",disable:false},
-      {id:4,link:'/tranport/import',title:"Import",disable:false},
-      {id:5,link:'/tranport/report',title:"Report",disable:false},
+      // { id:0,link:'/tranport/buses-management',title:"Buses Management",disable:true},
+      {id:0,link:'/tranport/auto-setup',title:"Auto Setup",disable:true},
+      {id:1,link:'/tranport/detail-list',title:"Detail List",disable:false},
+      {id:2,link:'/tranport/notice-change',title:"Notice Change",disable:false},
+      {id:3,link:'/tranport/import',title:"Import",disable:false},
+      {id:4,link:'/tranport/report',title:"Report",disable:false},
     ]
   }
 
-  selectBtn(id){
+  selectBtn(id:any){
     this.tabList[this.currRoute].disable=false;
     this.currRoute=id;
     this.tabList[id].disable=true;
